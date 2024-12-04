8d5d88e5c30806898ab44cfc8765d9ff3d42ad4c
Derssmnemashima <maniquizjayvee25@gmail.com>
2024-12-04 21:10:00 +0800

    Waitings

--- src/Page/Check_Out.jsx
+++ src/Page/Check_Out.jsx
@@ -241,7 +241,7 @@ const Check_Out = () => {
                                   <p>Apply referral points discount</p>
                                     <input  
                                         type="checkbox"  
-                                        checked={isradio} 
+                                        checked={isRadio} 
                                         onChange={() => setRadio(prev => !prev)} 
                                         className='cursor-pointer' 
                                         disabled={TotalsAll < 3000 || allPoints.totalReferred < 10} 
